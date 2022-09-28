87157fa6ef3dad74bfa84261f7695bede0b4dfab
George <georgea.fotheringham@gmail.com>
2022-09-28 17:45:03 -0400

    sets item incrementer for cart

--- shopping-cart/src/components/shoppingCart.js
+++ shopping-cart/src/components/shoppingCart.js
@@ -1,7 +1,17 @@
+import React, { useState } from "react";
+
 const ShoppingCart = () => {
+    const [ itemsNum, setItemsNum ] = useState(0);
+
+    const incrementItems = () => {
+        setItemsNum(itemsNum + 1);
+    }
+
     return (
         <div>
             <h1>Shopping Cart</h1>
+            <div>Items In Cart: {itemsNum}</div>
+            <button type='button' onClick={incrementItems}>Increment</button>
         </div>
     )
 }; 
